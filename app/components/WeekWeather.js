9994b1c0503ef311bf897c29fd8b32b2ee3b0447
kirill <bv_1514@mail.ru>
2017-06-20 18:14:14 +0300

    Modified daily weather

--- app/components/WeekWeather.js
+++ app/components/WeekWeather.js
@@ -2,51 +2,57 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  Image,
   ListView,
   View,
 } from 'react-native';
 
 import api from './../utilities/api';
 
+let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
 class WeekWeather extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      dailyWeather: [],
+      dailyWeather: ds,
       cityName: '',
     }
   }
 
   componentWillMount() {
     api.getDailyWeather().then((response) => {
-      let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
       this.setState({
         dailyWeather: ds.cloneWithRows(response.list),
         cityName: response.city.name,
       })
+    })
+    .catch((error) => {
+      console.error(error);
     });
   }
 
   render() {
 
-    return(
-				<View style={styles.container}>
-        // <Text>{this.state.cityName}</Text>
-        // <ListView
-        //   dataSource={this.state.dailyWeather}
-        //   renderRow={(rowData) => <Text>{rowData.speed}</Text>}
-        // />
+		return(
+      <View style={{backgroundColor: '#f0f76a'}}>
         <Text>{this.state.cityName}</Text>
+        <View>
+          <ListView
+            dataSource={this.state.dailyWeather}
+            renderRow={(rowData) =>
+              <View>
+                <Text>{Math.round(rowData.temp.day - 273)} C</Text>
+                <Image source={'http://openweathermap.org/img/w/' + rowData.weather[0].icon + '.png'} style={{width:30, height: 30}}/>
+              </View>
+            }
+          />
+        </View>
       </View>
 		);
 	}
 }
-
-const styles= StyleSheet.create({
-  container:{
-    flex: 1,
-  },
-});
 
 export default WeekWeather;
