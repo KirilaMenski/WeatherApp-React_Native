68b0ac5c004f66ae6a4fd84089624dbe109bbd4c
kirill <bv_1514@mail.ru>
2017-06-20 16:02:39 +0300

    Second view for daily weather

--- app/components/WeekWeather.js
+++ app/components/WeekWeather.js
@@ -2,18 +2,51 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  ListView,
   View,
 } from 'react-native';
 
+import api from './../utilities/api';
+
 class WeekWeather extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dailyWeather: [],
+      cityName: '',
+    }
+  }
+
+  componentWillMount() {
+    api.getDailyWeather().then((response) => {
+      let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.setState({
+        dailyWeather: ds.cloneWithRows(response.list),
+        cityName: response.city.name,
+      })
+    });
+  }
+
   render() {
 
-		return(
-      <View>
-        <Text>Hello, World!</Text>
+    return(
+				<View style={styles.container}>
+        // <Text>{this.state.cityName}</Text>
+        // <ListView
+        //   dataSource={this.state.dailyWeather}
+        //   renderRow={(rowData) => <Text>{rowData.speed}</Text>}
+        // />
+        <Text>{this.state.cityName}</Text>
       </View>
 		);
 	}
 }
+
+const styles= StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+});
 
 export default WeekWeather;
--- app/utilities/api.js
+++ app/utilities/api.js
@@ -3,7 +3,14 @@ var api  = {
       var  url = 'http://api.openweathermap.org/data/2.5/weather?id=625144&appid=6682c14b913c161756463200a81ce3b5';
       return fetch(url)
       .then((response) => response.json());
-  }
+  },
+
+  getDailyWeather() {
+    var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?id=625144&appid=6682c14b913c161756463200a81ce3b5';
+    return fetch(url)
+    .then((response) => response.json());
+  },
+
 };
 
 module.exports = api;
