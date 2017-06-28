1e1761fc20a78d9fb28ce07416c592557126fbb6
kirill <bv_1514@mail.ru>
2017-06-28 17:35:14 +0300

    Added carousel

--- app/components/WeekWeather.js
+++ app/components/WeekWeather.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Carousel from 'react-native-snap-carousel';
 
 import {
   StyleSheet,
@@ -9,6 +10,7 @@ import {
   View,
   ViewPagerAndroid,
   ActivityIndicator,
+  Dimensions,
   TouchableOpacity,
 } from 'react-native';
 
@@ -17,6 +19,14 @@ import date from './../utilities/date'
 import Root from './Root';
 
 let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+var pages = [];
+
+const horizontalMargin = 20;
+const slideWidth = 280;
+
+const sliderWidth = Dimensions.get('window').width;
+const itemWidth = slideWidth + horizontalMargin * 2;
+const itemHeight = 200;
 
 class WeekWeather extends Component {
 
@@ -25,6 +35,7 @@ class WeekWeather extends Component {
     this.state = {
       dailyWeather: ds,
       cityName: '',
+      dataSource: [],
       isLoading: true,
 
       weather: [],
@@ -44,6 +55,7 @@ class WeekWeather extends Component {
       this.setState({
         dailyWeather: ds.cloneWithRows(response.list),
         cityName: response.city.name,
+        dataSource: response.list,
         isLoading: false,
 
         weather:response.list[0].weather,
@@ -69,6 +81,34 @@ class WeekWeather extends Component {
       );
     }
 
+    for(var i = 0; i < this.state.dataSource.length; i++){
+
+      var rowData = this.state.dataSource[i];
+
+      pages.push(
+
+        <TouchableOpacity key={i} keyval={i} onPress={() => this.openWeather(i)} style={styles.touchableOpacity}>
+              <Text style={[styles.font, styles.date]}>{date.getDate(rowData.dt)}</Text>
+              <View style={styles.containerView}>
+                <View style={styles.temp}>
+                  <Text style={styles.font}>Morning: {Math.round(rowData.temp.morn - 273)} C</Text>
+                  <Text style={styles.font}>Day: {Math.round(rowData.temp.day - 273)} C</Text>
+                  <Text style={styles.font}>Night: {Math.round(rowData.temp.night - 273)} C</Text>
+                </View>
+                <View style={styles.description}>
+                  <Text style={styles.font}>Pressure: {rowData.pressure} mm Hg</Text>
+                  <Text style={styles.font}>Humidity: {rowData.humidity} %</Text>
+                  <Text style={styles.font}>Wind: {rowData.speed} m/s</Text>
+                </View>
+              </View>
+              <Image source={{uri: 'http://openweathermap.org/img/w/' + rowData.weather[0].icon + '.png'}}
+                style={{width:80, height: 80}}/>
+        </TouchableOpacity>
+
+      );
+
+    }
+
 		return(
 
       <View style={styles.mainContainer}>
@@ -83,54 +123,53 @@ class WeekWeather extends Component {
             wind = {this.state.wind}
             date = {this.state.date}/>
 
-            <ListView
-              style={{flex: 1, backgroundColor:'#fff'}}
-              horizontal={true}
-              dataSource={this.state.dailyWeather}
-              renderRow={(rowData) =>
-                <TouchableOpacity onPress={()=>{
-                  this.setState({weather:rowData.weather,
-                    icon:rowData.weather[0].icon,
-                    description:rowData.description,
-                    temp:Math.round(rowData.temp.day - 273),
-                    pressure:rowData.pressure,
-                    humidity:rowData.humidity,
-                    wind:rowData.speed,
-                    date:date.getDate(rowData.dt),});
-                }}>
-                  <Text style={[styles.font, styles.date]}>{date.getDate(rowData.dt)}</Text>
-                  <View style={styles.containerView}>
-                    <View style={styles.temp}>
-                      <Text style={styles.font}>Morning: {Math.round(rowData.temp.morn - 273)} C</Text>
-                      <Text style={styles.font}>Day: {Math.round(rowData.temp.day - 273)} C</Text>
-                      <Text style={styles.font}>Night: {Math.round(rowData.temp.night - 273)} C</Text>
-                    </View>
-                    <View style={styles.description}>
-                      <Text style={styles.font}>Pressure: {rowData.pressure} mm Hg</Text>
-                      <Text style={styles.font}>Humidity: {rowData.humidity} %</Text>
-                      <Text style={styles.font}>Wind: {rowData.speed} m/s</Text>
-                    </View>
-                    <Image source={{uri: 'http://openweathermap.org/img/w/' + rowData.weather[0].icon + '.png'}}
-                      style={{width:50, height: 50}}/>
-                  </View>
-                </TouchableOpacity>
-              }
-            />
+          <Carousel ref={(carousel) => { this._carousel = carousel; }}
+            sliderWidth={sliderWidth}
+            itemWidth={itemWidth}>
+
+            {pages}
+
+          </Carousel>
+
       </View>
 		);
 	}
 
+  openWeather(index){
+    // alert('Index' + index)
+    var rowData = this.state.dataSource[index - 1];
+    this.setState({weather:rowData.weather,
+      icon:rowData.weather[0].icon,
+      description:rowData.description,
+      temp:Math.round(rowData.temp.day - 273),
+      pressure:rowData.pressure,
+      humidity:rowData.humidity,
+      wind:rowData.speed,
+      date:date.getDate(rowData.dt),});
+  }
+
 }
 
 const styles = StyleSheet.create({
+  touchableOpacity:{
+    flex: 1,
+    backgroundColor: '#6ee9ef',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    borderWidth: 2,
+    borderRadius: 20,
+    borderColor: 'black',
+    margin: 2,
+  },
   mainContainer:{
     flex: 1,
+    backgroundColor: '#ededed',
   },
   containerView:{
     alignItems: 'center',
     flexDirection: 'row',
     padding: 10,
-    borderRightWidth: 1,
   },
   temp:{
     flex:1,
@@ -153,3 +192,36 @@ const styles = StyleSheet.create({
 });
 
 export default WeekWeather;
+// <ListView
+//   style={{flex: 1, backgroundColor:'#fff'}}
+//   horizontal={true}
+//   dataSource={this.state.dailyWeather}
+//   renderRow={(rowData) =>
+//     <TouchableOpacity onPress={()=>{
+//       this.setState({weather:rowData.weather,
+//         icon:rowData.weather[0].icon,
+//         description:rowData.description,
+//         temp:Math.round(rowData.temp.day - 273),
+//         pressure:rowData.pressure,
+//         humidity:rowData.humidity,
+//         wind:rowData.speed,
+//         date:date.getDate(rowData.dt),});
+//     }}>
+//       <Text style={[styles.font, styles.date]}>{date.getDate(rowData.dt)}</Text>
+//       <View style={styles.containerView}>
+//         <View style={styles.temp}>
+//           <Text style={styles.font}>Morning: {Math.round(rowData.temp.morn - 273)} C</Text>
+//           <Text style={styles.font}>Day: {Math.round(rowData.temp.day - 273)} C</Text>
+//           <Text style={styles.font}>Night: {Math.round(rowData.temp.night - 273)} C</Text>
+//         </View>
+//         <View style={styles.description}>
+//           <Text style={styles.font}>Pressure: {rowData.pressure} mm Hg</Text>
+//           <Text style={styles.font}>Humidity: {rowData.humidity} %</Text>
+//           <Text style={styles.font}>Wind: {rowData.speed} m/s</Text>
+//         </View>
+//         <Image source={{uri: 'http://openweathermap.org/img/w/' + rowData.weather[0].icon + '.png'}}
+//           style={{width:50, height: 50}}/>
+//       </View>
+//     </TouchableOpacity>
+//   }
+// />
